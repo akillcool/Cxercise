cfd108f07dceb0b7b5d91ef66a5f4130831616a3
akillcool <akillcool@outlook.com>
2016-05-15 14:20:13 +0800

    array2d - 处理二维数组的函数

--- myFirstC/myFirstC.cpp
+++ myFirstC/myFirstC.cpp
@@ -1,37 +1,71 @@
+//array2d - 处理二维数组的函数
+
 #include <stdio.h>
-double show(int, const double[]);
+#define ROWS 3
+#define COLS 4
+
+void sum_rows(int ar[][COLS], int rows);
+void sum_cols(int[][COLS], int);	//省略变量名称
+int sum2d(int(*ar)[COLS], int rows);	//另一种语法形式
 
 int main(void)
 {
-	int i;
-	double array[5] = { 4.5, 3.8, 8.9, 2.1, 5.6 };
+	int junk[ROWS][COLS] = {
+		{2, 4, 6, 8},
+		{3, 5, 7, 9},
+		{12, 10, 8, 6}
+	};
 
-	for ( i = 0; i < 5; i++)
-	{
-		printf("%.1lf\t", array[i]);
-	}
-	printf("\nThe substract of max and min is %.1lf\n", show(5, array));
+	sum_rows(junk, ROWS);
+	sum_cols(junk, ROWS);
+	printf("Sum of all elements = %d\n", sum2d(junk, ROWS));
 
 	while (true);
 	return 0;
 }
 
-double show(int n, const double ar[])
+void sum_rows(int ar[][COLS], int rows)
 {
-	int i;
-	double max;
-	double min;
+	int r;
+	int c;
+	int tot;
 
-	max = ar[0];
-	min = ar[0];
-
-	for ( i = 0; i < n; i++)
+	for ( r = 0; r < rows; r++)
 	{
-		if (max < ar[i])
-			max = ar[i];
-		if (min > ar[i])
-			min = ar[i];
+		tot = 0;
+		for ( c = 0; c < COLS; c++)
+		{
+			tot += ar[r][c];
+		}
+		printf("row %d: sum = %d\n", r, tot);
 	}
-	return max - min;
 }
 
+void sum_cols(int ar[][COLS], int rows)
+{
+	int r;
+	int c;
+	int tot;
+
+	for ( c = 0; c < COLS; c++)
+	{
+		tot = 0;
+		for ( r = 0; r < rows; r++)
+		{
+			tot += ar[r][c];
+		}
+		printf("col %d; sum = %d\n", c, tot);
+	}
+}
+
+int sum2d(int(*ar)[COLS], int rows)
+{
+	int c;
+	int r;
+	int tot = 0;
+
+	for (r = 0; r < rows; r++)
+		for (c = 0; c < COLS; c++)
+			tot += ar[r][c];
+	return tot;
+}
