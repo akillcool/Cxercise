1ea182e0ee74e1a6d8a0db344ad0b62a7dcdfe92
akillcool <akillcool@outlook.com>
2016-05-15 14:37:09 +0800

    PE 10-6

--- myFirstC/myFirstC.cpp
+++ myFirstC/myFirstC.cpp
@@ -4,9 +4,7 @@
 #define ROWS 3
 #define COLS 4
 
-void sum_rows(int ar[][COLS], int rows);
-void sum_cols(int[][COLS], int);	//省略变量名称
-int sum2d(int(*ar)[COLS], int rows);	//另一种语法形式
+void show2d(int[][COLS], int rows);
 
 int main(void)
 {
@@ -16,56 +14,21 @@ int main(void)
 		{12, 10, 8, 6}
 	};
 
-	sum_rows(junk, ROWS);
-	sum_cols(junk, ROWS);
-	printf("Sum of all elements = %d\n", sum2d(junk, ROWS));
+	show2d(junk, ROWS);
 
 	while (true);
 	return 0;
 }
 
-void sum_rows(int ar[][COLS], int rows)
-{
-	int r;
-	int c;
-	int tot;
-
-	for ( r = 0; r < rows; r++)
-	{
-		tot = 0;
-		for ( c = 0; c < COLS; c++)
-		{
-			tot += ar[r][c];
-		}
-		printf("row %d: sum = %d\n", r, tot);
-	}
-}
-
-void sum_cols(int ar[][COLS], int rows)
-{
-	int r;
-	int c;
-	int tot;
-
-	for ( c = 0; c < COLS; c++)
-	{
-		tot = 0;
-		for ( r = 0; r < rows; r++)
-		{
-			tot += ar[r][c];
-		}
-		printf("col %d; sum = %d\n", c, tot);
-	}
-}
-
-int sum2d(int(*ar)[COLS], int rows)
+void show2d(int ar[][COLS], int rows)
 {
 	int c;
 	int r;
-	int tot = 0;
 
 	for (r = 0; r < rows; r++)
+	{
 		for (c = 0; c < COLS; c++)
-			tot += ar[r][c];
-	return tot;
+			printf("%d\t", ar[r][c]);
+		printf("\n");
+	}
 }
