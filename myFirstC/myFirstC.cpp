d1ade677a1c0f0f5b23465f92feae4b433fd5480
akillcool <akillcool@outlook.com>
2016-05-15 13:20:07 +0800

    PE-10.4

--- myFirstC/myFirstC.cpp
+++ myFirstC/myFirstC.cpp
@@ -1,42 +1,38 @@
-// myFirstC.cpp : 定义控制台应用程序的入口点。
-//
-
 #include <stdio.h>
-void copy_arr(double[], double[], int);
-void copy_ptr(double *, double *, int);
+int show(int, double[]);
 
 int main(void)
 {
-	double source[5] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
-	double target1[5];
-	double target2[5];
+	int i;
+	double array[5] = { 4.5, 3.8, 8.9, 2.1, 5.6 };
 
-	copy_arr(source, target1, 5);
-	printf("\n");
-	copy_ptr(source, target2, 5);
+	for ( i = 0; i < 5; i++)
+	{
+		printf("%.1lf\t", array[i]);
+	}
+	printf("\nThe index of array's max number is %d.\n", show(5, array));
 
 	while (true);
 	return 0;
 }
 
-void copy_arr(double ar[], double target[], int n)
+int show(int n, double ar[])
 {
 	int i;
+	int max_ct;
+	double max;
+
+	max_ct = 0;
+	max = ar[0];
 
 	for ( i = 0; i < n; i++)
 	{
-		target[i] = ar[i];
-		printf("%.2lf\t", target[i]);
+		if (max < ar[i])
+		{
+			max = ar[i];
+			max_ct = i;
+		}
 	}
+	return max_ct;
 }
 
-void copy_ptr(double *ptr1, double *ptr2, int n)
-{
-	int i;
-
-	for ( i = 0; i < n; i++)
-	{
-		*(ptr2 + i) = *(ptr1 + i);
-		printf("%.2lf\t", *(ptr2 + i));
-	}
-}
