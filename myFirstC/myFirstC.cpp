f8341766728fc454460d9c55c14c151a8091ac0b
akillcool <akillcool@outlook.com>
2016-05-17 23:37:11 +0800

    命令行参数

--- myFirstC/myFirstC.cpp
+++ myFirstC/myFirstC.cpp
@@ -1,48 +1,14 @@
+//ÃüÁîĞĞ²ÎÊı
 #include <stdio.h>
-void show(int[][5], int);
-void dou(int[][5], int);
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	int source[3][5] = {
-		{1, 2, 3, 4, 5},
-		{6, 7, 8, 9, 0},
-		{3, 4, 5, 6, 7},
-	};
+	int count;
 
-	show(source, 3);
+	printf("The command line has %d arguments: \n", argc - 1);
+	for ( count = 1; count < argc; count++)
+	{
+		printf("%d: %s\n", count, argv[count]);
+	}
 	printf("\n");
-	dou(source, 3);
-	show(source, 3);
-
-
-	while (true);
-	return 0;
-}
-
-void show(int ar[][5], int rows)
-{
-	int i, j;
-
-	for ( i = 0; i < rows; i++)
-	{
-		for ( j = 0; j < 5; j++)
-		{
-			printf("%d\t", ar[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void dou(int ar[][5], int rows)
-{
-	int i, j;
-
-	for ( i = 0; i < rows; i++)
-	{
-		for ( j = 0; j < 5; j++)
-		{
-			ar[i][j] *= 2;
-		}
-	}
 }
