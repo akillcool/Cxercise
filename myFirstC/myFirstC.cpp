1234dac6ab7cbffd6dbc5f4369a5317dadc66c7c
akillcool <akillcool@outlook.com>
2016-05-20 20:09:34 +0800

    PE 11-9

--- myFirstC/myFirstC.cpp
+++ myFirstC/myFirstC.cpp
@@ -1,32 +1,44 @@
 #include <stdio.h>
-void reverse(char *p);
+#include <ctype.h>
+
+void non_space(char *);
+
 int main(void)
 {
 	char str[81];
-	do
+
+	printf("Please input a string: \n");
+	while (gets_s(str) && *str != '\0')
 	{
-		puts("input a string:");
-		gets_s(str);
-		reverse(str);
+		non_space(str);
 		puts(str);
-		puts("input any char except q to go on.");
-		gets_s(str);
-	} while (*str != 'q');
-	puts("Quit.");
+		printf("Please input another string, enter to quit. \n");
+	}
+
+	while (true);
 	return 0;
 }
 
-void reverse(char *p)
+void non_space(char *str)
 {
-	int i, n;
-	char temp;
-	for (n = 0; *(p + n) != '\0'; n++) 
-		continue;
-	n--;
-	for (i = 0; i < n - i; i++)
+	int n, i, j;
+	int ct = 0;		//count space
+
+	for (n = 0; *(str + n) != '\0'; n++)	//tell the length of string
 	{
-		temp = p[i];
-		p[i] = p[n - i];
-		p[n - i] = temp;
+		continue;
 	}
+
+	for (i = 0; i < n - ct; i++)
+	{
+		if (isspace(str[i]))
+		{
+			ct++;
+			for ( j = i; str[j + 1] != '\0'; j++)
+			{
+				str[j] = str[j + 1];
+			}
+		}
+	}
+	str[n - ct] = '\0';
 }
