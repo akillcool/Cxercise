b992d19489ddd6ae43ca8d069c4a1283face6e23
akillcool <akillcool@outlook.com>
2016-05-23 11:40:15 +0800

    PE 11-13

--- myFirstC/myFirstC.cpp
+++ myFirstC/myFirstC.cpp
@@ -1,11 +1,24 @@
+/* Programming Exercise 11-13 */
 #include <stdio.h>
+#include <stdlib.h>        /* for atof()           */
+#include <math.h>        /* for pow()            */
+/* #include <console.h> */   /* Macintosh adjustment */
 int main(int argc, char *argv[])
 {
-	for (int i = argc - 1; i > 0; i--)
-	{
-		printf("%s ", argv[i]);
-	}
+	double num;
+	int exp;
 
+	/* argc = ccommand(&argv); */  /* Macintosh adjustment */
+	if (argc != 3)
+		printf("Usage: %s number exponent\n", argv[0]);
+	else
+	{
+		num = atof(argv[1]);
+		exp = atoi(argv[2]);
+		printf("%.2f to the %d power = %g\n", num, exp, pow(num, exp));
+	}
+	
 	while (true);
 	return 0;
 }
+
