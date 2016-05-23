a4b4be5557ffef16c9727838d37d82df61eccf7e
akillcool <akillcool@outlook.com>
2016-05-23 11:32:52 +0800

    PE 11-12

--- myFirstC/myFirstC.cpp
+++ myFirstC/myFirstC.cpp
@@ -1,40 +1,10 @@
-/* pe11-11.c -- counts words and certain characters */
-/* Programming Exercise 11-11                       */
 #include <stdio.h>
-#include <ctype.h>       // for isspace()  
-#include <stdbool.h>     // for bool, true, false          
-int main(void)
+int main(int argc, char *argv[])
 {
-	char c;               // read in character 
-	int low_ct = 0;       // number of lowercase characters     
-	int up_ct = 0;        // number of uppercase characters     
-	int dig_ct = 0;       // number of digits          
-	int n_words = 0;      // number of words 
-	int punc_ct = 0;      // number of punctuation marks        
-	bool inword = false;  // == true if c is in a word  
-
-	printf("Enter text to be analyzed (EOF to terminate):\n");
-	while ((c = getchar()) != EOF)
+	for (int i = argc - 1; i > 0; i--)
 	{
-		if (islower(c))
-			low_ct++;
-		else if (isupper(c))
-			up_ct++;
-		else if (isdigit(c))
-			dig_ct++;
-		else if (ispunct(c))
-			punc_ct++;
-		if (!isspace(c) && !inword)
-		{
-			inword = true;  // starting a new word 
-			n_words++;      // count word          
-		}
-		if (isspace(c) && inword)
-			inword = false; // reached end of word
+		printf("%s ", argv[i]);
 	}
-	printf("\nwords = %d, lowercase = %d, uppercase = %d, "
-		"digits = %d, punctuation = %d\n",
-		n_words, low_ct, up_ct, dig_ct, punc_ct);
 
 	while (true);
 	return 0;
