9c8857757fc0e8a34c0950ecb5fcba5b8601ca14
akillcool <akillcool@outlook.com>
2016-07-07 19:48:59 +0800

    reducto.c 压缩文本

--- myFirstC/Hanoi.cpp
+++ myFirstC/Hanoi.cpp
@@ -1,31 +1,54 @@
-//count.c 使用标准IO
+//reducto.c 压缩文本
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define LEN 40
 
 int main(int argc, char *argv[])
 {
+	FILE *in, *out;
 	int ch;
-	FILE *fp;
-	long count = 0;
+	char name[LEN];
+	int count = 0;
 
-	if (argc != 2)
+	//检查命令行参数
+	if (argc < 2)
 	{
-		printf("usage: %s filename\n", argv[0]);
+		fprintf(stderr, "Usage: %s filename.\n", argv[0]);
 		exit(1);
 	}
-	if ((fp = fopen(argv[1], "r")) == NULL)
-	{
-		printf("Can't open %s", argv[1]);
-		exit(1);
-	}
-	while ((ch = getc(fp)) != EOF)
-	{
-		putc(ch, stdout);
-		count++;
-	}
-	fclose(fp);
-	printf("File %s has %ld characters\n", argv[1], count);
 
+	//实现输入
+	if ((in = fopen(argv[1], "r")) == NULL)
+	{
+		fprintf(stderr, "I couldn't open the file \"%s\"", argv[1]);
+		exit(2);
+	}
+
+	//实现输出
+	strcpy(name, argv[1]);
+	strcat(name, ".red");
+	if ((out = fopen(name, "w")) == NULL)
+	{
+		fprintf(stderr, "Can't creat output file.");
+		exit(3);
+	}
+
+	//复制数据
+	while ((ch = getc(in)) != EOF)
+	{
+		if (count++ % 3 == 0)
+		{
+			putc(ch, out);
+		}
+	}
+
+	//关闭文件
+	if (fclose(in)!=0||fclose(out)!=0)
+	{
+		fprintf(stderr, "Error in closing files.");
+	}
 	return 0;
 }
 
