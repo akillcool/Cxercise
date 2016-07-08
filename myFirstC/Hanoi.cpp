b5504e566e8764cfa365f712a376a2f8e3c46f78
akillcool <akillcool@outlook.com>
2016-07-08 20:06:29 +0800

    randombin.c 随机存取，二进制IO

--- myFirstC/Hanoi.cpp
+++ myFirstC/Hanoi.cpp
@@ -1,81 +1,56 @@
-//append.c - 把多个文件的内容追加到一个文件中
+//randombin.c 随机存取，二进制IO
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define BUFSIZE 1024
-#define SLEN 81
+#define ARSIZE 1000
 
-void append(FILE *source, FILE *dest);
 int main(void)
 {
-	FILE *fa, *fs;	//fa指向追加的目的文件，fs是源文件
-	int files = 0;	//追加的文件个数
-	char file_app[SLEN];	//目的文件名称
-	char file_src[SLEN];	//源文件名称
+	double numbers[ARSIZE];
+	double value;
+	const char *file = "numbers.dat";
+	int i;
+	long pos;
+	FILE *iofile;
 
-	puts("Enter name of destination file: ");
-	gets_s(file_app);
-
-	if ((fa = fopen(file_app, "a")) == NULL)		//打开目的文件
+	//创建一组double类型的值
+	for (i = 0; i < ARSIZE; i++)
 	{
-		fprintf(stderr, "Can't open %s\n", file_app);
-		exit(2);
+		numbers[i] = 100.0*i + 1.0 / (i + 1);
+	}
+	//尝试打开文件
+	if ((iofile = fopen(file, "wb")) == NULL)
+	{
+		fprintf(stderr, "Couldn't open %s for output.\n", file);
+		exit(1);
+	}
+	//把数组中的数据以二进制写进文件中
+	fwrite(numbers, sizeof(double), ARSIZE, iofile);
+	fclose(iofile);
+
+	if ((iofile = fopen(file, "rb")) == NULL)
+	{
+		fprintf(stderr, "Couldn't open %s for random access.\n", file);
+		exit(1);
 	}
 
-	if (setvbuf(fa, NULL, _IOFBF, BUFSIZE) != 0)	//创建输出缓冲区
+	//从文件中读取所选项目
+	printf("Enter the index in the range 0-%d.\n", ARSIZE - 1);
+	scanf("%d", &i);
+	while (i >= 0 && i < ARSIZE)
 	{
-		fputs("Can't create output buffer.", stderr);
-		exit(3);
+		pos = (long)i * sizeof(double);		//计算偏移量
+		fseek(iofile, pos, SEEK_SET);		//定位
+		fread(&value, sizeof(double), 1, iofile);
+		printf("The value there is %f.\n", value);
+		printf("Next index(out of range to quit.)\n");
+		scanf("%d", &i);
 	}
-
-	puts("Enter name of first source file(empty line to quit.)");
-	while (gets_s(file_src) && file_src[0] != '\0')
-	{
-		if (strcmp(file_app, file_src) == 0)	//比较两个文件，相同则跳过
-		{
-			fputs("Can't append file to itself.\n", stderr);
-		}
-		else if ((fs = fopen(file_src, "r")) == NULL)
-		{
-			fprintf(stderr, "Can't open %s\n", file_src);
-		}
-		else
-		{
-			if (setvbuf(fs, NULL, _IOFBF, BUFSIZE) != 0)	//创建输入缓冲区
-			{
-				fputs("Can't create input buffer", stderr);
-				continue;
-			}
-			append(fs, fa);
-			if (ferror(fs) != 0)	//判断是否读取错误
-			{
-				fprintf(stderr, "Error in reading file %s\n", file_src);
-			}
-			if (ferror(fa) != 0)
-			{
-				fprintf(stderr, "Error in writing file %s\n", file_app);
-			}
-			fclose(fs);
-			files++;
-			printf("File %s appended.\n", file_src);
-			puts("Next file(empty line to quit):");
-		}
-	}
-	printf("Done! %d files appended.\n", files);
-	fclose(fa);
+	fclose(iofile);
+	puts("Bye!");
 
 	return 0;
 }
 
-void append(FILE *source, FILE *dest)
-{
-	size_t bytes;
-	static char temp[BUFSIZE];		//分配一次
 
-	while ((bytes = fread(temp, sizeof(char), BUFSIZE, source)) > 0)
-	{
-		fwrite(temp, sizeof(char), bytes, dest);
-	}
-}
 
