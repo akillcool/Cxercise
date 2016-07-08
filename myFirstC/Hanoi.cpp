20bd79c05e9c504b0f26a585083e484169b976a3
akillcool <akillcool@outlook.com>
2016-07-08 21:05:34 +0800

    13.10.5

--- myFirstC/Hanoi.cpp
+++ myFirstC/Hanoi.cpp
@@ -1,41 +1,51 @@
-//13.10.4
+//13.10.5
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define BUF 256
+
+bool has_ch(char ch, char *line);
 
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	int count = 0;
-	double num;
-	double total = 0;
+	char ch;
+	char line[BUF];
 
-	if (argc == 1)
+	if (argc != 3)
 	{
-		fp = stdin;
-	}
-	else if (argc == 2)
-	{
-		if ((fp = fopen(argv[1], "r")) == NULL)
-		{
-			fprintf(stderr, "Failed to open %s.\n", argv[1]);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		fprintf(stderr, "Usage: %s filename",argv[0]);
+		fprintf(stderr, "Usage: %s [string] [filename]", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	printf("Now input numbers: (q to quit)\n");
-	while ((fscanf(fp,"%lf", &num)) == 1)
+	ch = argv[1][0];
+	printf("Now print line that has same string below:\n");
+	if ((fp = fopen(argv[2], "r")) == NULL)
 	{
-		total += num;
-		count++;
+		fprintf(stderr, "Failed to open %s.\n", argv[2]);
+		exit(EXIT_FAILURE);
 	}
-	printf("The average number of your input is %.2f.\n", total / count);
+	while ((fgets(line, BUF, fp)) != NULL)		//TODO
+	{
+		if (has_ch(ch,line))
+		{
+			fputs(line, stdout);
+		}
+	}
 
 	return 0;
 }
 
+bool has_ch(char ch, char *line)
+{
+	while (*line)
+	{
+		if (ch == *line++)
+		{
+			return true;
+		}
+	}
+	return false;
+}
 
 
